bedb8a4b6557ebab1e6731a436bae8cb2f5c159f
priyadharshini-B265 <priyabalakrishnan265@gmail.com>
2023-11-20 18:52:59 +0530

    Updated file

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class RewardValue {
+public class RewardValue {
+    public double cashValues;
+    public static double ConversionRate = 0.0035;
+    public RewardValue(double cashValue){
+        this.cashValues = cashValue;
+    }
+    public RewardValue(int milesValue){
+
+        this.cashValues = convertToCash(milesValue);
+    }
+    private static int convertToMiles(double cashValues){
+
+        return (int) (cashValues/ConversionRate);
+    }
+    private static double convertToCash(int milesValue){
+
+        return milesValue*ConversionRate;
+    }
+    public double getMilesValue(){
+
+        return convertToMiles(this.cashValues);
+    }
+    public double getCashValues(){
+
+        return cashValues;
+    }
 }
